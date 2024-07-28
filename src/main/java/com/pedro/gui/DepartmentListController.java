b7eb89a060cd92993ba09f2b95bc7e5dd1c9d33a
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-28 19:56:49 -0300

    Save new department

--- src/main/java/com/pedro/gui/DepartmentListController.java
+++ src/main/java/com/pedro/gui/DepartmentListController.java
@@ -86,6 +86,7 @@ public class DepartmentListController implements Initializable {
 			Pane pane = loader.load();
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
