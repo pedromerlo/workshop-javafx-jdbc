c0a1ed94f588be66e4942b7db726a7301d526d4a
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-28 08:12:48 -0300

    About view

--- src/main/java/com/pedro/App.java
+++ src/main/java/com/pedro/App.java
@@ -12,20 +12,25 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-
+    private static Scene mainScene;
     @Override
     public void start(@SuppressWarnings("exports") Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource("gui/MainView.fxml"));
         ScrollPane scrollPane = loader.load();
         scrollPane.setFitToHeight(true);
         scrollPane.setFitToWidth(true);
-        Scene mainScene = new Scene(scrollPane);
+        mainScene = new Scene(scrollPane);
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("Sample JavaFX application");
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    @SuppressWarnings("exports")
+    public static Scene getMainScene(){
+        return mainScene;
+    }
+
+    public final static void main(String[] args) {
         launch();
     }
 
